a4928f1c4d2642a97e84ba31337c22153f509600
Станислав Вашурин <stan.vv75@gmail.com>
2022-08-28 19:11:05 +0300

    Добавьте файлы проекта.

---
+++ StanVV75.HelloWorld1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StanVV75.HelloWorld1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Wake up, Neo...");
+            Console.ReadKey();
+            Console.Clear();
+            Console.WriteLine("The Matrix has you...");
+            Console.ReadKey();
+            Console.Clear();
+            Console.WriteLine("Follow the white rabbit.");
+            Console.ReadKey();
+            Console.Clear();
+            Console.WriteLine("Knock, knock, Neo.");
+            Console.ReadKey();
+            Console.Clear();
+        }
+    }
+}
